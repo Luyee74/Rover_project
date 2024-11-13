7b6d7445312aa84852373d41992c1f0fe408f031
corentin-verrier <151041160+corentin-verrier@users.noreply.github.com>
2024-11-13 09:05:52 +0100

    ajout functions.H.C
    ajout des fichiers et structures de base

---
+++ functions.h
@@ -0,0 +1,26 @@
+typedef struct s_cell{
+    int value; //valeur de la cellule
+    int next; //adresse de la cellule suivante
+}t_cell;
+
+typedef struct s_ht_list
+{
+    t_cell *head;   	// adresse de la première cellule
+    t_cell *tail;	// adresse de la dernière cellule
+} t_ht_list;
+
+
+typedef struct s_node{
+    int value; //valeur de la feuille
+    int nb_sons; //nombre de fils (7 car 7 déplacements)
+    t_ht_list sons; //adresses des fils
+}t_node;
+
+typedef struct t_tree{
+    t_node root;
+};
+
+#ifndef UNTITLED1_FUNCTIONS_H
+#define UNTITLED1_FUNCTIONS_H
+
+#endif //UNTITLED1_FUNCTIONS_H
