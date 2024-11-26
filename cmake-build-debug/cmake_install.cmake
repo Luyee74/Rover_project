c012a9ecc85c1feeab153b39c4032d763954cefb
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2024-11-26 01:48:51 +0100

    Ajout fiche de rendu final

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/cocov/OneDrive/Documents/GitHub/Rover_project
+# Install script for directory: C:/Users/tonyv/OneDrive/Documents/GitHub/Rover_project/Rover_project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/cocov/OneDrive/Documents/GitHub/Rover_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/tonyv/OneDrive/Documents/GitHub/Rover_project/Rover_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
